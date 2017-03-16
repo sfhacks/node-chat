1127c572326ae473ea6e8a5f43720d1be9def0dd
wchern <w.chern.dev@gmail.com>
2017-03-15 21:31:04 -0700

    Should work now

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//test
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,4 +11,8 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+});
+
+http.listen(port, function(){
+  console.log('listening on port ' + port);
 });
